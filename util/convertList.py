f923a137d0ad2ead8e9d7cb343a6b8c71644b6e2
MARTIKRISO <mshkodrov@gmail.com>
2023-12-07 19:25:06 +0100

    Fixed the regex on the script and the manifest for v1.0.9

--- util/convertList.py
+++ util/convertList.py
@@ -1,9 +1,9 @@
 import re
 
 global data
-with open("list_from_thunderstore.txt", "r") as f:
+with open("util\\list_from_thunderstore.txt", "r") as f:
     data = f.readlines()
 
-out = ", \n".join([f'"{f.strip()}"' for f in data if not re.match(r'(?:Coven)', f)])
+out = ", \n".join([f'"{f.strip()}"' for f in data if not re.match(r"TheCoven-TheCovenModpack-[0-9]\.[0-9]\.[0-9]", f)])
 
 print(out)
